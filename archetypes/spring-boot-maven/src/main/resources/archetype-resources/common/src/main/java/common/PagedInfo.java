6797d1dad1be0723631735561fadd1fe400c477a
wangwei <wangwei@B-T7HH9PHX-1908.local>
2024-09-27 10:03:29 +0800

    update common

---
+++ archetypes/spring-boot-maven/src/main/resources/archetype-resources/common/src/main/java/common/PagedInfo.java
@@ -0,0 +1,23 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.common;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class PagedInfo {
+    long pageSize;
+    long pageIndex;
+    long itemCount;
+    long itemTotal;
+
+    public static PagedInfo empty() {
+        return new PagedInfo(0, 0, 0, 0);
+    }
+}
---
+++ archetypes/spring-boot-maven/src/main/resources/archetype-resources/common/src/test/java/common/utils/JsonUtilTest.java
@@ -0,0 +1,33 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.common.utils;
+
+
+import cn.zhxu.data.Mapper;
+import cn.zhxu.xjson.JsonKit;
+import lombok.Data;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class JsonUtilTest {
+
+    @Data
+    public static class User {
+        String name;
+        int age;
+    }
+
+    @Test
+    public void testJsonKit() {
+        String json = "{${symbol_escape}"name${symbol_escape}":${symbol_escape}"Jack${symbol_escape}",${symbol_escape}"age${symbol_escape}":20}";
+        Mapper mapper = JsonKit.toMapper(json);
+        assertEquals(mapper.size(), 2);
+
+        User user = JsonKit.toBean(User.class, json);
+        assertEquals(user.age, 20);
+
+        String jsonString = JsonKit.toJson(user);
+        assertEquals(jsonString, "{${symbol_escape}"name${symbol_escape}":${symbol_escape}"Jack${symbol_escape}",${symbol_escape}"age${symbol_escape}":20}");
+    }
+
+}
