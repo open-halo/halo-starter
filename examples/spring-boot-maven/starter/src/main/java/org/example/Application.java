fe3a8cb9e5d39cf8612c57a71644e33dc5c71f02
wangwei <wangwei@B-T7HH9PHX-1908.local>
2024-10-09 17:01:20 +0800

    update

--- examples/spring-boot-maven/starter/src/main/java/org/example/Application.java
+++ examples/spring-boot-maven/starter/src/main/java/org/example/Application.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    public static void main(String[] args) {SpringApplication.run(Application.class, args);
     }
 
 //    private void unusedVariable() {
