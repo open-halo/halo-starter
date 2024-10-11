5f90fa45ba45ae27df57b7a9eb5ca259a25bedbe
wangwei <wangwei@B-T7HH9PHX-1908.local>
2024-10-11 23:14:09 +0800

    update archetype

---
+++ archetypes/spring-boot-maven/src/main/resources/archetype-resources/starter/src/main/java/k8s/ProbeController.java
@@ -0,0 +1,32 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.k8s;
+
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class ProbeController {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/startup")
+    public String startup() {
+        jdbcTemplate.execute("SELECT 1;");
+        return "OK";
+    }
+
+    @GetMapping("/liveness")
+    public String liveness() {
+        return "OK";
+    }
+
+    @GetMapping("/readiness")
+    public String readiness() {
+        return "OK";
+    }
+}
