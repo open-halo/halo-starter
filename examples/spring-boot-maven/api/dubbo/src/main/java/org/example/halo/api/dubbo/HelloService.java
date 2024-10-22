6bd62e8f60eddb7348e0f3a61d495a5fcf88aa58
wangwei <wangwei@B-T7HH9PHX-1908.local>
2024-10-22 17:57:57 +0800

    udpate

---
+++ examples/spring-boot-maven/api/dubbo/src/main/java/org/example/halo/api/dubbo/HelloService.java
@@ -0,0 +1,5 @@
+package org.example.halo.api.dubbo;
+
+public interface HelloService {
+    String sayHello(String name);
+}
--- examples/spring-boot-maven/starter/src/main/java/org/example/Application.java
+++ examples/spring-boot-maven/starter/src/main/java/org/example/Application.java
@@ -13,7 +13,12 @@ public class Application {
     public static void main(String[] args) {SpringApplication.run(Application.class, args);
     }
 
-//    private void unusedVariable() {
+    private void unusedVariable() {
 //        int unused = 42;
-//    }
+//        try {
+//
+//        } catch (Exception ex) {
+//
+//        }
+    }
 }
