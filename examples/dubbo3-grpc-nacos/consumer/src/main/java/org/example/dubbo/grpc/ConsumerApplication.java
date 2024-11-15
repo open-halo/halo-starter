08a86a91d448b3b87edb2ace2a0378097541e5e8
wangwei <wangwei@B-T7HH9PHX-1908.local>
2024-11-15 14:15:38 +0800

    add dubbo3 example

---
+++ examples/dubbo3-grpc-nacos/consumer/src/main/java/org/example/dubbo/grpc/ConsumerApplication.java
@@ -0,0 +1,14 @@
+package org.example.dubbo.grpc;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableDubbo
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
---
+++ examples/dubbo3-grpc-nacos/provider/src/main/java/org/example/dubbo/grpc/provider/GreeterImpl.java
@@ -0,0 +1,20 @@
+package org.example.dubbo.grpc.provider;
+
+import org.apache.dubbo.config.annotation.DubboService;
+import org.apache.dubbo.springboot.demo.idl.DubboGreeterTriple;
+import org.apache.dubbo.springboot.demo.idl.GreeterReply;
+import org.apache.dubbo.springboot.demo.idl.GreeterRequest;
+
+@DubboService
+public class GreeterImpl extends DubboGreeterTriple.GreeterImplBase {
+//    private static final Logger LOGGER = LoggerFactory.getLogger(GreeterImpl.class);
+
+    @Override
+    public GreeterReply greet(GreeterRequest request) {
+//        LOGGER.info("Server received greet request {}" , request);
+        return GreeterReply.newBuilder()
+                .setMessage("hello, " + request.getName() + "this is dubbo3!")
+                .build();
+    }
+
+}
